5b7b17ff8d2760f3e71d52dac6280d337e774dd9
marcia1997 <74060334+marcia1997@users.noreply.github.com>
2023-11-29 12:24:12 +0200

    update image port

--- src/data.js
+++ src/data.js
@@ -11,7 +11,7 @@ export const products = [
   },
   {
     id: 3,
-    img: "https://lh3.googleusercontent.com/vUCmiJfwL0tyCZi-wFg-bHCfuEMeLOE7xU4J-iT-O2NFNI1idBENKladmzK2dLcrMwuyUlVMmylbEbT4U38AhFpD5wEArUx74zz96wzh-8eNNuGZmEK50fklKneK5EyWndtQVwgDD4sfWymsIU4Fn8dQEdau1CeZhBXxmdaPWeXiKwIii_VjDMjBHsJRvffNJQLcq3fpxTISBEQBRwO34vrfpa23Zsxi6pOUPIVVAMdbKA_FX2BW-93PvHacRq1rppYW6DJr49II6vUPjLqARZnRhVzCYA_6-XscT6PWpMSOvo9yIO9-UzN2_ojgubbMnxvcV9f1BO3xsspuS49KVjvWI-u4KPHwOeFANNkxO4Bb-crPWwYuKeF3plh_MtOr4GZEg8mgIMoEMFcRc6r7OFv6cjI3UnJIl6o4sGiXyp69btd8al6EZNdyBl-mJBEnK4wYyxas50TEnOZuQSqpOvAqHvhf4xMMzXIRvrERPzzLYoO6Da3cu3DUNJxBGGPTVAjZLyFa8GjoBMhVo1OXfglbm3cUsA4EI7EptsEKo4--9ZqO76w4UFkb3cdrfD5eSTx4t5yAOwE4N_hfXg7CcWJrjuNVTwJmo0lbj7gWIOUcE0_AL2Z1prLTevm5Umm8UxO7T6OD0ypsmuuDAI5FuuUn3RDI_-eSliN2OgYyNmObJ6_bplDNDQmqLKxKWtizhnwIcUi9yGQ_NhdfAsGmn2AKWPqkUXZU0Q0kbv3-vGJ0u40CIJsglTeGgumz7vRWLVm6HNpE-lFV73isIAWAkDFnl3raKiJYdog=w235-h625-no?authuser=0",
-    link: "https://www.figma.com/file/KJskMAuqdd7PbfUrRWl4G9/Untitled?type=design&node-id=0%3A1&mode=design&t=LVinOZxCag0rtPJS-1",
+    img: require( "./img/deco-shop.png"),
+    link: "https://github.com/marcia1997/deco-shop",
   },
 ];
