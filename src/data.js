39a55c51977e9254d4400743242e39384d4dc7ae
marcia1997 <74060334+marcia1997@users.noreply.github.com>
2023-11-29 11:42:30 +0200

    update image

--- src/data.js
+++ src/data.js
@@ -6,7 +6,7 @@ export const products = [
   },
   {
     id: 2,
-    img: "https://lh3.googleusercontent.com/ihr9bQwXKdcM0NKPvVFiTDcDqURbALoUP34wsyw8lhrMXu_GIYBnyH_DK6Q71Vf_jXtdLjl5qxTKNYu3F6c4wSqz6kb4y4V6ICudsvZqU7kifvWvx1AC-B8Z2optUOoR8zvHU71vgMAaO4OYaebh1gnGy6KeGLzkHEkKaasGv6G_Qo4fxPtE8Qa743oAXuDvo__zF4IS1p7-C5gYyacqqaaUkhHT_UMgonS4QVDdc1_ut3BjtT9RQUr8RQ-83shFJLUdMml30mtGdS87UrViitLgZJP-CuAkxTMlKOrBN1-H4q4pbpn74Ez-Fbc3OQ4-dtBv4LgyJcNnSG_WHViiGWKwKtRA7mmRYH9VQLlTCNoej3NnHUtcMw776fvpWnju-IBXZR6WUkcAmjWY3oJzHIdOnMMiQ7X5Djm7Gd8Vw8J_RvLS5V4v05x_VfR_ok5wy06Xpq8rKMLF5SB_i8ahLdHryxWlAsn562jEo8WQCOp7WK4Wh8mxaEJwA4wKHjzsQzYkkhvoneQntPkOKWjzb9j3B86qiDI_Ji80RXvWURnGGIDPJHe_-czEWhRAbjgIZEqdkGq2HxJrA0xVbeLdBjq22CigWz34KQ0zM44vjYr1Wn48QyNB_0GeAKTFZABdy2TDAhwbaI6E2r8_5DPngsmQ4yRLaCs3J60DlKooJ9quIOJw2-oT-7RLACWFmSQ9xt1l5ld6tZCqnPoFYvWN-d7I9wKSqgAKjwVSHN70DYnoRIR1vnRHrxEqfHGQxh8aNaGsC4jrl8rZxkYnFeAgmxDf3cJgrOpAfq8=w333-h625-no?authuser=0",
+    img :require( "./img/wecooking-1.png"),
     link: "https://github.com/marcia1997/wecooking-todel",
   },
   {
