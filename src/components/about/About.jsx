cde77e5449481b07573c3f3e617fe27eeeab3417
marcia1997 <74060334+marcia1997@users.noreply.github.com>
2025-03-20 10:49:49 +0200

    Update About.jsx

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -3,25 +3,11 @@ import React from "react";
 import { Helmet } from "react-helmet";
 
 
-import Award1 from "../../img/award1.png";
-import Award2 from "../../img/award2.png";
-import Award3 from "../../img/award3.png";
-import Award4 from "../../img/award4.png";
-import Award5 from "../../img/award5.png";
-import Award6 from "../../img/award6.png";
-import Award7 from "../../img/award7.png";
-import Award8 from "../../img/award8.png";
-import photo from "../../img/profile.png";
 
 const About = () => {
   return (
     <div className="a">
-      <div className="a-left">
-        <div className="a-card bg"></div>
-        <div className="a-card">
-        <img src={photo} alt=""className="i-img-profile"/>
-        </div>
-      </div>
+     
       <div className="a-right">
         <h2 className="a-title">
         <Helmet>
@@ -32,41 +18,22 @@ const About = () => {
       </Helmet>
           About me</h2>
         <p className="a-sub">
-        I love to design and create proyects,ideas and dreams.
-            <br/>The most powerful thing is giving them life through my hands.
+        I love designing and creating projects, ideas, and dreams.<br/>
+        The most powerful thing is bringing them to life through my hands.            
+        
         </p>
         <p className="a-desc">
-         I considere myself an self-thought person.<br></br>
-         I enjoy learning new things and challanging with.
-         Learning differents aspects of coding and programming gave me an opportunity of discover myself.<br></br>
-         During my journey I enjoy how amazing is the power of generate something unique only with my computer.<br></br>
-         I attend different courses and trainings,going from MERN stack,Python and UI/UX which gave me a big vision and skills about the creation of products,focused in the code side and also  the user needs.<br>
-         </br>
-         I still have a lot to learn I believe perserverance and hunger of knowledge are my main strengths.
+        I consider myself a self-taught person.
+        I enjoy learning new things and challenging myself.<br/> Exploring different aspects of coding and programming has given me the opportunity to discover more about myself.<br/>
+        Throughout my journey, I’ve been fascinated by the power of creating something unique using only my computer.
+
+        <br/>I have attended various courses and training programs, covering the MERN stack, Python, and UI/UX. <br/>These experiences have given me a broad vision and valuable skills in product creation, focusing on both the technical side and user needs.<br/>
+
+        I still have a lot to learn, but I believe perseverance and a hunger for knowledge are my greatest strengths.
          
         </p>
         
-        {/*
-        <div className="tecnologies">
-        <div className="container-1">
-          
-        <div className="a-award">
-          <img src={Award1} alt="" className="a-award-img"/>
-          <img src={Award2} alt="" className="a-award-img" />
-          <img src={Award3} alt="" className="a-award-img" />
-          <img src={Award4} alt="" className="a-award-img" />
-         </div>
-        </div>
-        <div className="container-2">
-        <div className="a-award">
-          <img src={Award5} alt="" className="a-award-img" />
-          <img src={Award6} alt="" className="a-award-img" />
-          <img src={Award7} alt="" className="a-award-img" />
-          <img src={Award8} alt="" className="a-award-img" />
-         </div>
-        </div>
-      </div> 
-  */}
+  
       </div>
       </div>
   );
