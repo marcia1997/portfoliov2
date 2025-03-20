266a05c1ddb835dcdbf7be3558aa33b0c4f263dd
marcia1997 <74060334+marcia1997@users.noreply.github.com>
2025-03-20 09:59:06 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import React from "react";
-import About from "./components/about/About";
 import Contact from "./components/contact/Contact";
 import Intro from "./components/intro/Intro";
 import ProductList from "./components/productList/ProductList";
 import Footer from "./components/footer/Footer";
+import About from "./components/about/About";
 
 //Comment test
 const App = () => {
